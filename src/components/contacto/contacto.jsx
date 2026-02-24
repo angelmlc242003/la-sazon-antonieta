4305be60775b049fb3fead31d9d2e175adba7545
angelmlc242003 <angelmlc200324@gmail.com>
2026-02-23 23:04:42 -0300

    Fix: contacto CSS import casing

--- src/components/contacto/contacto.jsx
+++ src/components/contacto/contacto.jsx
@@ -1,6 +1,6 @@
 // src/components/contacto/contacto.jsx
 import React, { useState, useEffect } from "react";
-import "./Contacto.css";
+import "./contacto.css";
 
 import MailIcon from "../../assets/icons/mail.svg?react";
 import PhoneIcon from "../../assets/icons/phone.svg?react";
